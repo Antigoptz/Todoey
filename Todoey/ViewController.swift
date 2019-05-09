5f540d56a8ffe51ddc5a58ebe7b3e90085bad527
Igor Antonov <igor.antonov@valamis.com>
2019-05-09 16:50:11 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Igor Antonov on 09/05/2019.
+//  Copyright © 2019 Igor Antonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
